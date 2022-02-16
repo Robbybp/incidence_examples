94197f7ff2ee273b5795653049b9445ed0abac0c
Robert Parker <robbybparker@gmail.com>
2022-02-16 12:32:17 -0500

    initialize empty package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="incidence_examples",
+    description=(
+        "Examples using basic graph algorithms to work with algebraic "
+        "models in Pyomo."
+    ),
+    author="Robert Parker",
+    author_email="rparker1@andrew.cmu.edu",
+    license="BSD-3",
+    packages=["incidence_examples"],
+)
