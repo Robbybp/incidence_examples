6384102f6b6b481af9fc1a31691e6c1aa4be8d4a
Robert Parker <robbybparker@gmail.com>
2022-02-23 18:08:48 -0500

    actually test solver termination status

--- incidence_examples/example2/tests/test_run_example2.py
+++ incidence_examples/example2/tests/test_run_example2.py
@@ -1,4 +1,5 @@
 import pyomo.common.unittest as unittest
+import pyomo.environ as pyo
 
 import incidence_examples.example2.run_scc_example as example_script
 
@@ -7,6 +8,11 @@ class TestRunExample(unittest.TestCase):
 
     def test_solve_status(self):
         res1, res2 = example_script.main(nxfe=2)
+        self.assertIs(
+            res1.solver.termination_condition,
+            pyo.TerminationCondition.infeasible,
+        )
+        pyo.assert_optimal_termination(res2)
 
 
 if __name__ == "__main__":
