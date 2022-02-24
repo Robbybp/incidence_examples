3891e3d7fe9d2ebeac8fc8520c45fa72e998855f
Robert <robbybparker@gmail.com>
2022-02-23 21:30:34 -0500

    test matching in final solve from tutorial

---
+++ incidence_examples/tutorial/tests/test_run_tutorial.py
@@ -0,0 +1,19 @@
+import pyomo.common.unittest as unittest
+
+import incidence_examples.tutorial.run_tutorial as tutorial_script
+
+
+class TestRunTutorial(unittest.TestCase):
+
+    def test_incidence(self):
+
+        igraph = tutorial_script.main()
+        M = len(igraph.constraints)
+        N = len(igraph.variables)
+        matching = igraph.maximum_matching()
+        self.assertEqual(M, N)
+        self.assertEqual(N, len(matching))
+
+
+if __name__ == "__main__":
+    unittest.main()
